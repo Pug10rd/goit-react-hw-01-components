102cd657594954ff60cf70a02906b2b8e7b50be7
Ivan Zarichnyi <91795849+Pug10rd@users.noreply.github.com>
2023-03-22 18:24:51 +0100

    commit-5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Profile } from './Profile/profile';
-import user from './Profile/user.json';
+import { Profile } from './user-profile/user-profile';
+import user from './user-profile/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from '../components/Statistics/data.json';
 import { FriendList } from './FriendList/FriendList';
